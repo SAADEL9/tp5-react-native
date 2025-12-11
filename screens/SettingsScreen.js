ff583e5b7e13cbc206a47ce61366541286d9315f
SAADEL9 <155107889+SAADEL9@users.noreply.github.com>
2025-12-11 12:34:32 +0100

    Initial commit

---
+++ screens/SettingsScreen.js
@@ -0,0 +1,8 @@
+ import { View, Text, Button } from 'react-native'; 
+export default function SettingsScreen() { 
+  return ( 
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}> 
+      <Text>⚙  Paramètres</Text> 
+    </View> 
+  ); 
+} 
